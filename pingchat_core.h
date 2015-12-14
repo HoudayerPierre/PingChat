198c166b3a073b443cc7add5f3b5502d55514936
HOUDAYER Pierre <pierro.houdayer@laposte.net>
2015-12-14 23:41:32 +0100

    doxygen

--- pingchat_core.h
+++ pingchat_core.h
@@ -4,19 +4,38 @@
 #include <QMainWindow>
 
 namespace Ui {
-class PingChat_Core;
+    class PingChat_Core;
 }
 
+/*!
+ * \file pingchat_core.h
+ *
+ * \class PingChat_Core
+ *
+ * \brief PingChat_Core core of PingChat application
+ *
+ * \author GUEDON Damien and HOUDAYER Pierre
+ *
+ * \version 0.1
+ *
+ * \date 14 Dec 2015
+ *
+ */
 class PingChat_Core : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    explicit PingChat_Core(QWidget *parent = 0);
-    ~PingChat_Core();
+    public:
+        /*!
+         * \fn bool PingChat_Core(QWidget *parent = 0)
+         *
+         * \brief Class builder of PingChat_Core.
+         */
+        explicit PingChat_Core(QWidget *parent = 0);
+        ~PingChat_Core();
 
-private:
-    Ui::PingChat_Core *ui;
+    private:
+        Ui::PingChat_Core *ui;
 };
 
 #endif // PINGCHAT_CORE_H
